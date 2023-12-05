e0a483dd48586b4ca618331420871b915d783c9f
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2023-12-05 18:02:04 +0100

    Create foodfarm.js

---
+++ foodfarm.js
@@ -0,0 +1,33 @@
+function updateResults() {
+    // Get selected products
+    const selectedProducts = Array.from(document.getElementById('products').selectedOptions).map(option => option.value);
+
+    // Simulate fetching matching farmers (replace with actual logic)
+    const matchingFarmers = getMatchingFarmers(selectedProducts);
+
+    // Display results
+    const resultsContainer = document.getElementById('results');
+    resultsContainer.innerHTML = `<h2>Matching Farmers:</h2>`;
+    
+    if (matchingFarmers.length === 0) {
+        resultsContainer.innerHTML += `<p>No matching farmers found.</p>`;
+    } else {
+        matchingFarmers.forEach(farmer => {
+            resultsContainer.innerHTML += `<p>${farmer}</p>`;
+        });
+    }
+}
+
+// Function to simulate fetching matching farmers (replace with actual logic)
+function getMatchingFarmers(selectedProducts) {
+    const farmersData = {
+        'John_Doe': ['Apples', 'Tomatoes', 'Carrots'],
+        'Jane_Smith': ['Potatoes', 'Strawberries', 'Lettuce'],
+        'Alex_Johnson': ['Corn', 'Peppers', 'Cucumbers'],
+    };
+
+    return Object.keys(farmersData).filter(farmer => {
+        const farmerProducts = farmersData[farmer];
+        return selectedProducts.some(product => farmerProducts.includes(product));
+    });
+}
