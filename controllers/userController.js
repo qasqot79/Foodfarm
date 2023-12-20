4adb37e8610d33bfbdceecdb4d8e7de06c9541f3
qasqot79 <cird2007@yahoo.com>
2023-12-20 16:19:47 +0000

    foodfarm

---
+++ controllers/userController.js
@@ -0,0 +1,10 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+  // Implementation for user registration
+};
+
+exports.login = async (req, res) => {
+  // Implementation for user login
+};
